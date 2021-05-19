3df78a73d3ed70a57f76d99f37bd3fc9072e8d77
Emily McCormick <emilyannmccormick@gmail.com>
2021-05-19 12:26:46 -0500

    get and post routes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,29 @@
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 
-const { createNewNote } = require('../../lib/notes');
-//const { animals } = require('../../data/animals');
 
-router.get('/api/notes', (req, res) => {
-    //need to update these
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
+const { createNewNote } = require('../lib/notes');
+const  notes = require('../db/db.json');
+
+// a route to get all the notes (to put on the side?)
+router.get('/notes', (req, res) => {
+    // console.log(notes);
+    return res.json(notes);
 });
 
-
-router.post('/api/notes', (req, res) => {
+// a route for adding a new note
+router.post('/notes', (req, res) => {
     // working on updating
   // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
+  req.body.id = uuidv4();
 
  
 const note = createNewNote(req.body, notes);
     res.json(note);
   
 });
+
+// BONUS: delete route and function (didn't do at time of development but would go here)
+//router.delete('/notes/:id')
 
 module.exports = router;
