f2d19b6b0eecafccba78208fdc868ed72ea5bbd1
Emily McCormick <emilyannmccormick@gmail.com>
2021-05-19 16:33:32 -0500

    add deployed link

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Note Taker - Module 11 Challenge
 
-deployed application:
+deployed application: https://shrouded-brushlands-88740.herokuapp.com/
 
 ## Description 
 An application that can be used to write and save notes. The app uses an Express.js back end and saves and retrieves note data from a JSON file.
 
 ## Visual
+![](./public/assets/images/demo.png)
+
 
 ## Technologies Used
 * Express.js
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const { createNewNote } = require('../lib/notes');
 const  notes = require('../db/db.json');
 
-// a route to get all the notes (to put on the side?)
+// a route to get all the notes (to put on the side)
 router.get('/notes', (req, res) => {
     // console.log(notes);
     return res.json(notes);
@@ -13,8 +13,8 @@ router.get('/notes', (req, res) => {
 
 // a route for adding a new note
 router.post('/notes', (req, res) => {
-    // working on updating
-  // set id based on what the next index of the array will be
+    
+  // set id 
   req.body.id = uuidv4();
 
  
